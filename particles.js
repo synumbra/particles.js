c7fc6282d1e706ace7fc43f846e0388ccb283482
Samuel Horwitz <samuel.horwitz@gmail.com>
2016-06-24 19:52:31 -0400

    typo fix

--- particles.js
+++ particles.js
@@ -254,18 +254,18 @@ var pJS = function(tag_id, params){
   pJS.fn.particle = function(color, opacity, position){
 
     /* size */
-    this.radius = (pJS.particles.size.random ? pnJS.fn.secureRand() : 1) * pJS.particles.size.value;
+    this.radius = (pJS.particles.size.random ? pJS.fn.secureRand() : 1) * pJS.particles.size.value;
     if(pJS.particles.size.anim.enable){
       this.size_status = false;
       this.vs = pJS.particles.size.anim.speed / 100;
       if(!pJS.particles.size.anim.sync){
-        this.vs = this.vs * pnJS.fn.secureRand();
+        this.vs = this.vs * pJS.fn.secureRand();
       }
     }
 
     /* position */
-    this.x = position ? position.x : pnJS.fn.secureRand() * pJS.canvas.w;
-    this.y = position ? position.y : pnJS.fn.secureRand() * pJS.canvas.h;
+    this.x = position ? position.x : pJS.fn.secureRand() * pJS.canvas.w;
+    this.y = position ? position.y : pJS.fn.secureRand() * pJS.canvas.h;
 
     /* check position  - into the canvas */
     if(this.x > pJS.canvas.w - this.radius*2) this.x = this.x - this.radius;
@@ -283,7 +283,7 @@ var pJS = function(tag_id, params){
     if(typeof(color.value) == 'object'){
 
       if(color.value instanceof Array){
-        var color_selected = color.value[Math.floor(pnJS.fn.secureRand() * pJS.particles.color.value.length)];
+        var color_selected = color.value[Math.floor(pJS.fn.secureRand() * pJS.particles.color.value.length)];
         this.color.rgb = hexToRgb(color_selected);
       }else{
         if(color.value.r != undefined && color.value.g != undefined && color.value.b != undefined){
@@ -305,9 +305,9 @@ var pJS = function(tag_id, params){
     }
     else if(color.value == 'random'){
       this.color.rgb = {
-        r: (Math.floor(pnJS.fn.secureRand() * (255 - 0 + 1)) + 0),
-        g: (Math.floor(pnJS.fn.secureRand() * (255 - 0 + 1)) + 0),
-        b: (Math.floor(pnJS.fn.secureRand() * (255 - 0 + 1)) + 0)
+        r: (Math.floor(pJS.fn.secureRand() * (255 - 0 + 1)) + 0),
+        g: (Math.floor(pJS.fn.secureRand() * (255 - 0 + 1)) + 0),
+        b: (Math.floor(pJS.fn.secureRand() * (255 - 0 + 1)) + 0)
       }
     }
     else if(typeof(color.value) == 'string'){
@@ -316,12 +316,12 @@ var pJS = function(tag_id, params){
     }
 
     /* opacity */
-    this.opacity = (pJS.particles.opacity.random ? pnJS.fn.secureRand() : 1) * pJS.particles.opacity.value;
+    this.opacity = (pJS.particles.opacity.random ? pJS.fn.secureRand() : 1) * pJS.particles.opacity.value;
     if(pJS.particles.opacity.anim.enable){
       this.opacity_status = false;
       this.vo = pJS.particles.opacity.anim.speed / 100;
       if(!pJS.particles.opacity.anim.sync){
-        this.vo = this.vo * pnJS.fn.secureRand();
+        this.vo = this.vo * pJS.fn.secureRand();
       }
     }
 
@@ -361,15 +361,15 @@ var pJS = function(tag_id, params){
       this.vx = velbase.x;
       this.vy = velbase.y;
       if(pJS.particles.move.random){
-        this.vx = this.vx * (pnJS.fn.secureRand());
-        this.vy = this.vy * (pnJS.fn.secureRand());
+        this.vx = this.vx * (pJS.fn.secureRand());
+        this.vy = this.vy * (pJS.fn.secureRand());
       }
     }else{
-      this.vx = velbase.x + pnJS.fn.secureRand()-0.5;
-      this.vy = velbase.y + pnJS.fn.secureRand()-0.5;
+      this.vx = velbase.x + pJS.fn.secureRand()-0.5;
+      this.vy = velbase.y + pJS.fn.secureRand()-0.5;
     }
 
-    // var theta = 2.0 * Math.PI * pnJS.fn.secureRand();
+    // var theta = 2.0 * Math.PI * pJS.fn.secureRand();
     // this.vx = Math.cos(theta);
     // this.vy = Math.sin(theta);
 
@@ -383,7 +383,7 @@ var pJS = function(tag_id, params){
     var shape_type = pJS.particles.shape.type;
     if(typeof(shape_type) == 'object'){
       if(shape_type instanceof Array){
-        var shape_selected = shape_type[Math.floor(pnJS.fn.secureRand() * shape_type.length)];
+        var shape_selected = shape_type[Math.floor(pJS.fn.secureRand() * shape_type.length)];
         this.shape = shape_selected;
       }
     }else{
@@ -581,19 +581,19 @@ var pJS = function(tag_id, params){
 
       if(p.x - p.radius > pJS.canvas.w){
         p.x = new_pos.x_left;
-        p.y = pnJS.fn.secureRand() * pJS.canvas.h;
+        p.y = pJS.fn.secureRand() * pJS.canvas.h;
       }
       else if(p.x + p.radius < 0){
         p.x = new_pos.x_right;
-        p.y = pnJS.fn.secureRand() * pJS.canvas.h;
+        p.y = pJS.fn.secureRand() * pJS.canvas.h;
       }
       if(p.y - p.radius > pJS.canvas.h){
         p.y = new_pos.y_top;
-        p.x = pnJS.fn.secureRand() * pJS.canvas.w;
+        p.x = pJS.fn.secureRand() * pJS.canvas.w;
       }
       else if(p.y + p.radius < 0){
         p.y = new_pos.y_bottom;
-        p.x = pnJS.fn.secureRand() * pJS.canvas.w;
+        p.x = pJS.fn.secureRand() * pJS.canvas.w;
       }
 
       /* out of canvas modes */
@@ -773,8 +773,8 @@ var pJS = function(tag_id, params){
           pJS.particles.color,
           pJS.particles.opacity.value,
           {
-            'x': pos ? pos.pos_x : pnJS.fn.secureRand() * pJS.canvas.w,
-            'y': pos ? pos.pos_y : pnJS.fn.secureRand() * pJS.canvas.h
+            'x': pos ? pos.pos_x : pJS.fn.secureRand() * pJS.canvas.w,
+            'y': pos ? pos.pos_y : pJS.fn.secureRand() * pJS.canvas.h
           }
         )
       )
@@ -1206,8 +1206,8 @@ var pJS = function(tag_id, params){
           dist = Math.sqrt(dx*dx + dy*dy);
 
       if(dist <= p1.radius + p2.radius){
-        p1.x = position ? position.x : pnJS.fn.secureRand() * pJS.canvas.w;
-        p1.y = position ? position.y : pnJS.fn.secureRand() * pJS.canvas.h;
+        p1.x = position ? position.x : pJS.fn.secureRand() * pJS.canvas.w;
+        p1.y = position ? position.y : pJS.fn.secureRand() * pJS.canvas.h;
         pJS.fn.vendors.checkOverlap(p1);
       }
     }
